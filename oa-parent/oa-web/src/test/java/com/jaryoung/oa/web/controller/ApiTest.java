d37e8200c0fc679976ad3de9e8227e1345f8c796
jerryoung <huige3344ai@gmail.com>
2021-02-28 15:17:58 +0800

    fix:配置修改

--- oa-parent/oa-web/src/test/java/com/jaryoung/oa/web/controller/ApiTest.java
+++ oa-parent/oa-web/src/test/java/com/jaryoung/oa/web/controller/ApiTest.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -20,7 +19,6 @@ public class ApiTest {
 
     @Resource
     private Api api;
-
 
     @Test
     public void createEmployee() {
